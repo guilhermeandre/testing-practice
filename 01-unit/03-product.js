859f1a56e70441e11e397173fc2b5404f1bc7fd9
Guilherme Andre dos Santos <guilhermeandredossantos@Guilhermes-iMac.local>
2020-04-06 11:00:35 -0700

    Unit tests added to product operation

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,10 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(num1, num2) {  
+  if (num1 === undefined) num1 = 0;
+  if (num2 === undefined) num2 = 0;
+  return num1 * num2;
 }
 
 // ==================================================
@@ -16,26 +18,35 @@ try {
   var result = product(2, 2);
   if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
 
+
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+  var result = product(2,0);
+  if (result !== 0) throw new Error('Expected product(2,0) to be 0. Received: ' + result);
+  var result = product(0,2);
+  if (result !== 0) throw new Error('Expected product(0,2) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(4,4,4,4,4);
+  if (result !== 16) throw new Error('Expected product(4,4,4,4,4) to be 16. Received: ' +result);
 
-  // --------------------------------------------------
+  // -------------------------------------------------- 
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = product(4);
+  if (result !== 0) throw new Error('Expected product(4) to be 0. Every number multiplied by 0 must be 0. Received: ' +result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = product();
+  if (result !== 0) throw new Error('Exepected product() to be 0. Received: ' +result)
   console.log('All tests passed successfully.');
 
 // ==================================================
